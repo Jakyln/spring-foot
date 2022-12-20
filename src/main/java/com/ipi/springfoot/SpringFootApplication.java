1ef3204cafa8bc4a36f464fe2c6ba42eb9370071
Julien <kalaycijulien@gmail.com>
2022-12-20 10:31:11 +0100

    Instanciation du projet

---
+++ src/main/java/com/ipi/springfoot/SpringFootApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.springfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipi/springfoot/SpringFootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi.springfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
